f94c20aa95b7306cbedafd33dc6579753cd94e92
pritam <pritam.pk36129@gmail.com>
2024-05-31 17:32:47 +0530

    remove unwanted imports and logic for cookie

--- src/main/java/com/hisab/hisab/security/AuthenticationFilter.java
+++ src/main/java/com/hisab/hisab/security/AuthenticationFilter.java
@@ -2,21 +2,16 @@ package com.hisab.hisab.security;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
-import java.nio.file.AccessDeniedException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -37,8 +32,15 @@ public class AuthenticationFilter extends OncePerRequestFilter {
             FilterChain filterChain)
             throws ServletException, IOException
     {
-        String token = request.getHeader("Authorization");
-        if (token == null) {
+        Cookie cookie = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie c : cookies) {
+            if (c.getName().equals("Authorization")) {
+                cookie = c;
+            }
+        }
+
+        if (cookie == null) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // Set the appropriate status code
             response.setContentType("application/json");
             String jsonResponse = String.format("{\"error\": \"%s\"}", "token not present");
@@ -46,7 +48,7 @@ public class AuthenticationFilter extends OncePerRequestFilter {
             return;
         }
 
-        JwtData jwtData = validateToken(token);
+        JwtData jwtData = validateToken(cookie.getValue());
         List<SimpleGrantedAuthority> authorities = new ArrayList<>();
         for(String role : jwtData.getRoles()) {
             authorities.add(new SimpleGrantedAuthority(role));
